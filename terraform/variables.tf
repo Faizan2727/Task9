e8d1877d3c4688627652d45f2dcc9541dd201a9b
FAIZAN SHAIKH <faizanshaikhbmw2727@gmail.com>
2025-08-05 15:31:53 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,17 @@
-variable "project_id" {}
-variable "region" {
-  default = "us-central1"
+variable "project_id" {
+  description = "GCP Project ID"
+  type        = string
+  default     = "trim-mote-464705-a0"
 }
-#variable "credentials_file" {}
+
+variable "region" {
+  description = "GCP Region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "credentials_file" {
+  description = "Path to the service account key file"
+  type        = string
+  default     = "credentials.json"
+}
