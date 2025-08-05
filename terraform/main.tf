3e6828470654cf42b7e96c1f7c05db0da43c801b
FAIZAN SHAIKH <faizanshaikhbmw2727@gmail.com>
2025-08-05 11:32:53 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,23 @@
+provider "google" {
+  credentials = file(var.credentials_file)
+  project     = var.project_id
+  region      = var.region
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "github-gke-cluster"
+  location = var.region
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "node-pool"
+  location   = var.region
+  cluster    = google_container_cluster.primary.name
+  initial_node_count = 1
+  node_config {
+    machine_type = "e2-medium"
+    oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
+}
